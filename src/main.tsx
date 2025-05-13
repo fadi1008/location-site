8df0a5db93065a5d8ffccc5a76798634e5e129eb
fadi 1008 <fadi.bellalah1008@gmail.com>
2025-05-13 17:05:51 +0100

    Mise à jour du health check pour Render

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Gardez seulement ceci pour empêcher le sleep
+setInterval(() => {
+  fetch('/').catch(() => {}); // On fetch la racine au lieu de /health
+}, 5 * 60 * 1000);
+
+createRoot(document.getElementById('root')!).render(<App />)
